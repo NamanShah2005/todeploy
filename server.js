8ed1f427d63cbbf778b2afb932b4809e1fcaba07
NamanShah2005 <namanhitenshah.com>
2024-01-12 21:55:32 +0530

    thirdone12

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-import lop from "./index.js"
-lop.listen(process.env.PORT, process.env.HOSTNAME, () => {
+import app from "./index.js"
+// const app = express()
+app.listen(process.env.PORT, process.env.HOSTNAME, () => {
     console.log(`The server is working on the port http://${process.env.HOSTNAME}:${process.env.PORT}/ in ${process.env.NODE_ENV} mode`)
 })
